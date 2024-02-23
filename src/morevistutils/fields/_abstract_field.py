bed49865101a349d7db40674d5b7cb8788157d05
AsgerJon <asgerjon2@gmail.com>
2024-02-23 17:30:00 +0100

    Initial commit

--- src/morevistutils/fields/_abstract_field.py
+++ src/morevistutils/fields/_abstract_field.py
@@ -5,11 +5,15 @@ from __future__ import annotations
 
 from typing import Callable, Any
 
+from vistutils.parse import maybe
+from vistutils.waitaminute import typeMsg
+
 
 class AbstractBaseField:
   """AbstractBaseField provides a baseclass for descriptor classes."""
 
   __owned_instances__ = {}
+  __hooked_methods__ = {}
 
   def __init__(self, *args, **kwargs) -> None:
     self.__field_name__ = None
@@ -41,10 +45,37 @@ class AbstractBaseField:
     instances of the descriptor class, methods decorated will be called
     only once. """
 
-  def HOOK(self, target: str, interFace: Callable = None) -> None:
+  def HOOK(self, target: str, interFace: Callable = None) -> Callable:
     """Methods decorated with this decorator, will apply 'interFace' to
     the callable named 'target' in the owner class. If 'decorated' is a
     method in the descriptor class, and 'original' is the method at the
-    target name in the owner class, then:
+    target name in the owner class, the following change will be applied
+    to the owner class during '__set_name__':
+    setattr(owner, target, interFace(decorated, original))
+    The decorated method is returned. Please note, that a copy of the
+    decorated class is passed to the owner class rather than a reference
+    to the original. """
 
-    setattr(owner, target, interFace(target, original))"""
+    def decorator(decorated: Callable) -> Callable:
+      """This decorator returns the replacement method. """
+      self.__hooked_methods__[target] = {'decorated': decorated,
+                                         'interFace': interFace}
+      return decorated
+
+    return decorator
+
+  def applyHooks(self, owner: type) -> None:
+    """This method applies all hooks to the instance. """
+    for target, hook in self.__hooked_methods__.items():
+      decorated = hook['decorated']
+      interFace = maybe(hook['interFace'], lambda *args: args[0])
+      if hasattr(owner, target):
+        original = getattr(owner, target)
+        if callable(original):
+          if hasattr(original, '__func__'):
+            original = original.__func__
+        else:
+          e = typeMsg('original', original, Callable)
+          raise TypeError(e)
+      else:
+        setattr(owner, target, decorated)

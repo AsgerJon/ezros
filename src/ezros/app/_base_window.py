93825cd22e0c9ca290662bd43dda1aa67f33f0d4
AsgerJon <asgerjon2@gmail.com>
2024-03-26 11:41:51 +0100

    Fixing icons

---
+++ src/ezros/app/_base_window.py
@@ -0,0 +1,12 @@
+"""BaseWindow provides the menus, menubar and statusbar for the main
+application."""
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ezside.windows import BaseWindow as BaseWindow_
+
+
+class BaseWindow(BaseWindow_):
+  """BaseWindow provides the menus, menubar and statusbar for the main
+  application."""
